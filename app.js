21c0043702ba7e6e1c4cc51e1236cfa8ff9a0c15
45deg <45deg@example.com>
2015-06-06 11:59:00 +0900

    spec5: initial commit

--- app.js
+++ app.js
@@ -2,14 +2,30 @@
 
 // モジュール読み込み
 var fs = require('fs');
+var rl = require('readline');
 
 (function main(){
     var filename = process.argv[2]; // argv = ["node", "app.js", filename]
-    if(!filename) { // ファイル名が与えられていない
+
+    if(typeof filename !== "string") { // ファイル名が与えられていない
         console.log("No Input File.");
         process.exit();
     }
 
-    var contents = fs.readFileSync(filename, 'utf-8');
-    process.stdout.write(contents); // 改行なしで出力
+    var inputStream = fs.createReadStream(filename);
+    // ファイル入力(非同期)のためのモジュール
+    var inputReadLine = rl.createInterface({'input': inputStream, 'output': {}});
+    // 一行づつ読み込むためのインターフェース
+
+    var recipies = []; // レシピを保存する
+
+    inputReadLine
+        .on('line', function(line){ // 一行読み込む
+            recipies.push(line); // レシピに追加
+        })
+        .on('close', function(){ // ファイル読み込み完了
+            recipies.forEach(function(recipie, index){
+                console.log((index + 1) + ':' + recipie); // 1-indexed
+            });
+        });
 })();
