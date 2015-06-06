08850d2af205455981788e005a2062f92f667382
45deg <45deg@example.com>
2015-06-06 11:25:59 +0900

    spec3: implemented

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("オムライス");
+"use strict"; // strict mode にする (cf. https://developer.mozilla.org/ja/docs/Web/JavaScript/Strict_mode)
+
+// モジュール読み込み
+var fs = require('fs');
+
+(function main(){
+    var filename = process.argv[2]; // argv = ["node", "app.js", filename]
+    var contents = fs.readFileSync(filename, 'utf-8');
+    console.log(contents);
+})();
